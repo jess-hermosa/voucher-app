26a6a5268347440bd8e2748056b3e44e5d5783d0
Jess Hermosa <hermosagens@gmail.com>
2023-10-09 19:28:04 +0800

    created layout components

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,95 @@
+const secondaryNavigation = [
+  { name: "Overview", href: "#", current: true },
+  { name: "Activity", href: "#", current: false },
+  { name: "Settings", href: "#", current: false },
+  { name: "Collaborators", href: "#", current: false },
+  { name: "Notifications", href: "#", current: false },
+];
+const stats = [
+  { name: "Number of deploys", value: "405" },
+  { name: "Average deploy time", value: "3.65", unit: "mins" },
+  { name: "Number of servers", value: "3" },
+  { name: "Success rate", value: "98.5%" },
+];
+
+function classNames(...classes: any) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Header = () => {
+  return (
+    <header>
+      {/* Secondary navigation */}
+      <nav className="flex overflow-x-auto border-b border-white/10 py-4">
+        <ul
+          role="list"
+          className="flex min-w-full flex-none gap-x-6 px-4 text-sm font-semibold leading-6 text-gray-400 sm:px-6 lg:px-8"
+        >
+          {secondaryNavigation.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.href}
+                className={item.current ? "text-indigo-400" : ""}
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </nav>
+
+      {/* Heading */}
+      <div className="flex flex-col items-start justify-between gap-x-8 gap-y-4 bg-gray-700/10 px-4 py-4 sm:flex-row sm:items-center sm:px-6 lg:px-8">
+        <div>
+          <div className="flex items-center gap-x-3">
+            <div className="flex-none rounded-full bg-green-400/10 p-1 text-green-400">
+              <div className="h-2 w-2 rounded-full bg-current" />
+            </div>
+            <h1 className="flex gap-x-3 text-base leading-7">
+              <span className="font-semibold text-white">Planetaria</span>
+              <span className="text-gray-600">/</span>
+              <span className="font-semibold text-white">mobile-api</span>
+            </h1>
+          </div>
+          <p className="mt-2 text-xs leading-6 text-gray-400">
+            Deploys from GitHub via main branch
+          </p>
+        </div>
+        <div className="order-first flex-none rounded-full bg-indigo-400/10 px-2 py-1 text-xs font-medium text-indigo-400 ring-1 ring-inset ring-indigo-400/30 sm:order-none">
+          Production
+        </div>
+      </div>
+
+      {/* Stats */}
+      <div className="grid grid-cols-1 bg-gray-700/10 sm:grid-cols-2 lg:grid-cols-4">
+        {stats.map((stat, statIdx) => (
+          <div
+            key={stat.name}
+            className={classNames(
+              statIdx % 2 === 1
+                ? "sm:border-l"
+                : statIdx === 2
+                ? "lg:border-l"
+                : "",
+              "border-t border-white/5 py-6 px-4 sm:px-6 lg:px-8"
+            )}
+          >
+            <p className="text-sm font-medium leading-6 text-gray-400">
+              {stat.name}
+            </p>
+            <p className="mt-2 flex items-baseline gap-x-2">
+              <span className="text-4xl font-semibold tracking-tight text-white">
+                {stat.value}
+              </span>
+              {stat.unit ? (
+                <span className="text-sm text-gray-400">{stat.unit}</span>
+              ) : null}
+            </p>
+          </div>
+        ))}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss";
 const config: Config = {
   content: [
     "./src/pages/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Shared/Components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [require("@tailwindcss/forms")],
