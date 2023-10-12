588087cc0095758882af716f087cdccecf9ecc83
Jess Hermosa <hermosagens@gmail.com>
2023-10-13 00:27:07 +0800

    added select and text area shared component

--- src/components/Shared/Select.tsx
+++ src/components/Shared/Select.tsx
@@ -1,37 +1,34 @@
-import { Fragment, useState } from "react";
+import { FC, Fragment } from "react";
 import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
-
-const people = [
-  { id: 1, name: "Wade Cooper" },
-  { id: 2, name: "Arlene Mccoy" },
-  { id: 3, name: "Devon Webb" },
-  { id: 4, name: "Tom Cook" },
-  { id: 5, name: "Tanya Fox" },
-  { id: 6, name: "Hellen Schmidt" },
-  { id: 7, name: "Caroline Schultz" },
-  { id: 8, name: "Mason Heaney" },
-  { id: 9, name: "Claudie Smitham" },
-  { id: 10, name: "Emil Schaefer" },
-];
 
 function classNames(...classes: any) {
   return classes.filter(Boolean).join(" ");
 }
 
-const Select = () => {
-  const [selected, setSelected] = useState(people[3]);
+type Option = {
+  id: string | number;
+  value: string;
+};
 
+interface Props {
+  options: Option[];
+  label: string;
+  selectedOption: Option;
+  onChange: (value: Option) => void;
+}
+
+const Select: FC<Props> = ({ options, label, selectedOption, onChange }) => {
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox value={selectedOption ?? options[0]} onChange={onChange}>
       {({ open }) => (
         <>
           <Listbox.Label className="block text-sm font-medium leading-6 text-gray-900">
-            Assigned to
+            {label}
           </Listbox.Label>
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-600 sm:text-sm sm:leading-6">
-              <span className="block truncate">{selected.name}</span>
+              <span className="block truncate">{selectedOption.value}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
                 <ChevronUpDownIcon
                   className="h-5 w-5 text-gray-400"
@@ -48,16 +45,16 @@ const Select = () => {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
+                {options.map((option: Option) => (
                   <Listbox.Option
-                    key={person.id}
+                    key={option.id}
                     className={({ active }) =>
                       classNames(
                         active ? "bg-indigo-600 text-white" : "text-gray-900",
                         "relative cursor-default select-none py-2 pl-3 pr-9"
                       )
                     }
-                    value={person}
+                    value={option}
                   >
                     {({ selected, active }) => (
                       <>
@@ -67,7 +64,7 @@ const Select = () => {
                             "block truncate"
                           )}
                         >
-                          {person.name}
+                          {option.value}
                         </span>
 
                         {selected ? (
--- src/components/Shared/TextArea.tsx
+++ src/components/Shared/TextArea.tsx
@@ -1,3 +1,27 @@
-const TextArea = () => {};
+import { FC, TextareaHTMLAttributes } from "react";
+
+interface Props extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  label?: string;
+}
+
+const TextArea: FC<Props> = ({ label, className, ...rest }) => {
+  return (
+    <div>
+      <label
+        htmlFor="comment"
+        className="block text-sm font-medium leading-6 text-gray-900"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <textarea
+          rows={4}
+          className={`${className} block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6`}
+          {...rest}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default TextArea;
