c43129a4b860485859501795fa6b59e996d245c3
Jess Hermosa <hermosagens@gmail.com>
2023-10-24 03:30:27 +0800

    set serializable check to false

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,6 +10,10 @@ const rootReducer = combineReducers({
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export type StateType = ReturnType<typeof rootReducer>;
