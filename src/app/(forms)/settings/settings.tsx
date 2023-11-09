07ba5b97c067732f5be941f076ec3a0e52f7a35c
jess-hermosa <hermosagens@gmail.com>
2023-11-09 21:28:10 +0800

    map default settings

--- src/app/(forms)/settings/settings.tsx
+++ src/app/(forms)/settings/settings.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Settings } from "@/common/backend-types";
 import { fetchSettings } from "@/server/api";
 import { useQuery } from "@tanstack/react-query";
 
@@ -21,34 +22,24 @@ const VoucherSettings = () => {
           </p>
 
           <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
-            <div className="pt-6 sm:flex">
-              <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                Default BIR account
-              </dt>
-              <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                <div className="text-gray-900">Tom Cook</div>
-                <button
-                  type="button"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Update
-                </button>
-              </dd>
-            </div>
-            <div className="pt-6 sm:flex">
-              <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
-                Default Cash MDS account
-              </dt>
-              <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
-                <div className="text-gray-900">tom.cook@example.com</div>
-                <button
-                  type="button"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Update
-                </button>
-              </dd>
-            </div>
+            {settings.map((setting: Settings) => (
+              <div className="pt-6 sm:flex">
+                <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">
+                  {setting.name}
+                </dt>
+                <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+                  <div className="text-gray-900">
+                    {setting.account?.code || ""}
+                  </div>
+                  <button
+                    type="button"
+                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                  >
+                    Update
+                  </button>
+                </dd>
+              </div>
+            ))}
           </dl>
         </div>
       </div>
